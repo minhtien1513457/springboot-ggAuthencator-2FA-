834fa3ea914af4cb6acb4c5702d06f2b713e2522
Tien Phan <tien.phan@hrs.com>
2024-03-18 09:44:57 +0700

    init

--- spring-google-authenticator-main/README.md
+++ spring-google-authenticator-main/README.md
@@ -6,3 +6,7 @@
 **USERS TO USE**
 1. alice@gmail.com
 2. kek@kek.com
+
+**User the new relic**
+1. mvn clean install(gerate the jar file)
+2. java -javaagent:.\newrelic-agent-8.9.1.jar -jar .\target\google-auth-demo-0.0.1-SNAPSHOT.jar
---
+++ spring-google-authenticator-main/src/main/java/com/vladm/googleauthdemo/config/MicrometerConfig.java
@@ -0,0 +1,64 @@
+package com.vladm.googleauthdemo.config;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//import com.newrelic.telemetry.Attributes;
+//import com.newrelic.telemetry.micrometer.NewRelicRegistry;
+//import com.newrelic.telemetry.micrometer.NewRelicRegistryConfig;
+
+import java.time.Duration;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.util.NamedThreadFactory;
+
+//@Configuration
+//@AutoConfigureBefore({ CompositeMeterRegistryAutoConfiguration.class, SimpleMetricsExportAutoConfiguration.class })
+//@AutoConfigureAfter(MetricsAutoConfiguration.class)
+//@ConditionalOnClass(NewRelicRegistry.class)
+public class MicrometerConfig {
+
+//    @Bean
+//    public NewRelicRegistryConfig newRelicConfig() {
+//        return new NewRelicRegistryConfig() {
+//            @Override
+//            public String get(String key) {
+//                return null;
+//            }
+//
+//            @Override
+//            public String apiKey() {
+//                return "3ba717f96dbf5c5d26479c8b5f8c7916bed4NRAL"; // for production purposes take it from config file
+//            }
+//
+//            @Override
+//            public Duration step() {
+//                return Duration.ofSeconds(5);
+//            }
+//
+//            @Override
+//            public String serviceName() {
+//                return "test2FA"; // for production purposes take it from config file
+//            }
+//
+//        };
+//    }
+//
+//    @Bean
+//    public NewRelicRegistry newRelicMeterRegistry(NewRelicRegistryConfig config) throws UnknownHostException {
+//        NewRelicRegistry newRelicRegistry = NewRelicRegistry.builder(config)
+//                .commonAttributes(new Attributes().put("host", InetAddress.getLocalHost().getHostName())).build();
+//        newRelicRegistry.config().meterFilter(MeterFilter.ignoreTags("plz_ignore_me"));
+//        newRelicRegistry.config().meterFilter(MeterFilter.denyNameStartsWith("jvm.threads"));
+//        newRelicRegistry.start(new NamedThreadFactory("newrelic.micrometer.registry"));
+//        return newRelicRegistry;
+//    }
+}
